0a0953d17d4ccd99895bba6216f37df1e17016bd
Erik Aybar <erikthedeveloper@gmail.com>
2014-07-11 07:43:28 -0600

    Still experimenting with Prophecy objects etc... - WIP
    Test(s) passing/broken
    - PHPSpec recognizing that classes don't exist. Not requesting to create them
    - See here: http://sylius.readthedocs.org/en/latest/contributing/code/bdd.html

--- src/ErikAybar/SendGridWeb/SendGridApi.php
+++ src/ErikAybar/SendGridWeb/SendGridApi.php
@@ -7,6 +7,11 @@ class SendGridApi
 
     protected $base_url = "https://api.sendgrid.com/api/";
 
+    public function __construct()
+    {
+        // TODO: write logic here
+    }
+
     public function getBaseUrl()
     {
         return $this->base_url;
